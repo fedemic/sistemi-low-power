8b94f4e928bd6ea8c2cf9913b19735a57bbd4b3e
federicomicelli <s233819@studenti.polito.it>
2020-03-28 21:03:25 +0100

    Update matlab HA, FA e RCA

---
+++ Lab_1/Matlab/HA_FA_Probability.m
@@ -0,0 +1,24 @@
+close all;
+clear all;
+clc;
+
+%% probabilità input
+P_A = 0.5;
+P_B = 0.5;
+P_Cin = 0.5;
+
+%% half adder
+
+P_sum_half = P_A*(1- P_B) + P_B*(1- P_A);
+A_sum_half = 2*P_sum_half*(1 - P_sum_half);
+
+P_cout_half = P_A*P_B;
+A_cout_half = 2*P_cout_half*(1 - P_cout_half);
+
+%% full adder
+
+P_sum_full = P_A*(1-P_B)*(1-P_Cin) + P_B*(1-P_A)*(1-P_Cin) + P_Cin*(1-P_A)*(1-P_B) + P_A*P_B*P_Cin;
+A_sum_full = 2*P_sum_full*(1-P_sum_full);
+    
+P_cout_full = P_A*P_B*(1-P_Cin) + P_A*P_Cin*(1-P_B) + P_B*P_Cin*(1-P_A) + P_A*P_B*P_Cin;
+A_cout_full = 2*P_cout_full*(1-P_cout_full);
--- Lab_1/Matlab/Ripple_Carry_Adder_Probability.m
+++ Lab_1/Matlab/Ripple_Carry_Adder_Probability.m
@@ -1,18 +1,18 @@
-%% Autore Tiziano
-
 close all;
 clear all;
 clc;
 
-% Vettori da intendere da LSB a MSB
-P_S = [0 0 0 0 0 0 0 0];
-A_S = [0 0 0 0 0 0 0 0];
-
+%% Vettori da intendere da LSB a MSB
 P_A = 0.5*ones(8,1);
 P_B = 0.5*ones(8,1);
 P_Cin = [0 0 0 0 0 0 0 0 0]; % Ha 9 elementi per poter inserire l'ultimo Cout
+
+P_S = [0 0 0 0 0 0 0 0];
+A_S = [0 0 0 0 0 0 0 0];
+
 A_Cout = [0 0 0 0 0 0 0 0];
 
+%% calcolo probabilità e activity
 for i = 1:8
     % Calcolo P_S e ESW
     P_S(i) = P_A(i)*(1-P_B(i))*(1-P_Cin(i)) + P_B(i)*(1-P_A(i))*(1-P_Cin(i)) + P_Cin(i)*(1-P_A(i))*(1-P_B(i)) + P_A(i)*P_B(i)*P_Cin(i);
@@ -25,8 +25,13 @@ for i = 1:8
     
 end
 
-% Risultati
-P_S
-A_S
-P_Cin
-A_Cout
+%% dati simulati
+clk_cycles = 200;
+
+% RCA con delay sulle somme
+A_S1_sim = [92 102 101 98 85 105 102 84]/clk_cycles;
+A_CO1_sim = 123/clk_cycles;
+
+% RCA con delay su somme e carry
+A_S2_sim = [92 178 201 210 199 213 242 250]/clk_cycles;
+A_CO2_sim = 123/clk_cycles;
