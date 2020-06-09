e36398c424ab69d949c92a1c3693dbafb24d263d
FrSp13 <francy.spanna@gmail.com>
2020-06-09 12:43:31 +0200

    Report

--- Report/code/Lab_3/D1_D2_D3.vhd
+++ Report/code/Lab_3/D1_D2_D3.vhd
@@ -1,3 +1,3 @@
-signal D1:	std_logic_vector(byte-1 downto 0) := ('0', others => '1'); -- 01111111
-signal D2:	std_logic_vector(0 to byte-1) := (others => '0'); -- 00000000
-signal D3:	std_logic_vector(byte-1 downto 0) := (others => '1'); -- 1111111
+	signal D1:std_logic_vector(byte-1 downto 0) := ('0', others => '1'); -- 01111111
+	signal D2:std_logic_vector(0 to byte-1) := (others => '0'); -- 00000000
+	signal D3:std_logic_vector(byte-1 downto 0) := (others => '1'); -- 1111111
